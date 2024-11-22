14e9b30e2ac34ab8cdde385b58ea61c96a531bff
Sammy <frnkverdam@gmail.com>
2024-11-22 13:07:57 +0100

    Commit

--- App1/App1/Program.cs
+++ App1/App1/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program program = new Program(); //Program is de variable
+            program.Run(); //De type is class
         }
+        static void function()
+        {
+            Console.WriteLine("Dit is nu de start van mijn programma")
+        }
+        // Function
+        internal void Run();
+           
     }
 }
